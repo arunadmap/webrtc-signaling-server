84e4cc9f1e9a9c806b9eba9de2d3efe874ff7be7
Aruna <arunadmap@icloud.com>
2023-04-28 01:09:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+     
+    }
+  });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('create-something', (msg) => {
+        io.emit('foo', msg);
+      console.log('message: ' + msg);
+    });
+  });
+
+server.listen(3030, () => {
+  console.log('listening on *:3030');
+});
